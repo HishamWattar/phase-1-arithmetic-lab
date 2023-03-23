73c2df3b740cbf7eb94b7917e33ffe58261b085c
hisham <h.wattar@netro.xyz>
2023-03-23 18:49:15 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num2 = 31;
+let num1 = 2;
+let multiply = num1 * num2;
+let random = Math.ceil(Math.random());
+let num3 = 4;
+let num4 = 1;
+let mod = num3 % num4 + 4
+let max = Math.max(1, 3, 4, 5, 20 ,19)
